0800aa89a52bcdce20f4bcddde36a11d142227f4
maldad <canond_7@hotmail.com>
2015-11-01 16:26:00 -0600

    lados OK, escalamiento OK, (w, s))

--- poligono2.cpp
+++ poligono2.cpp
@@ -1,32 +1,27 @@
 /*programa que dibuja poligonos regulares y ademas...
 TODO
-[] - Escalamiento con W y S
+[x] - Escalamiento con W y S
 [] - Rotacion con A y D
 [] - Traslacion usando las flechitas
-[] - Lados de 3 a 9 con teclas de número
+[x] - Lados de 3 a 9 con teclas de número
 */
 
 #include <GL/glut.h>
 #include <math.h>
 #include <stdio.h>
 
-const float radio = 0.7;
+const float radio = 0.5;
 float x, y;
 int lados = 3;
 float angle;// = 360/lados;
 float incAngle;// = 360/lados;
-
-void pedirLados(){
-    puts("Numero de lados: ");
-    scanf("%d", &lados);
-    angle = 360/lados;
-    incAngle = 360/lados;
-    return;
-}
+//variables de escalado
+float sx = 1.0;
+float sy = 1.0;
 
 //args: lados,
-void poligono(int l){
-    lados = l;
+void poligono(){
+    //lados = l;
     angle = 360/lados;
     incAngle = angle;
     glClear(GL_COLOR_BUFFER_BIT);
@@ -36,7 +31,9 @@ void poligono(int l){
     for(int i = 0; i < lados; i++){
         x = radio*cos(angle/57.3);
         y = radio*sin(angle/57.3);
-        //printf("%f %f %f\n", x, y, angle);
+        //aplicando escalado...
+        x = x * sx;
+        y = y * sy;
         glVertex2f(x, y);
         angle += incAngle;
     }//for
@@ -45,7 +42,7 @@ void poligono(int l){
 }//poligono
 
 void display(void){
-    poligono(lados);
+    poligono();
 }//display
 
 void reshape(int w, int h){
@@ -63,6 +60,7 @@ void keyboard(unsigned char key, int x, int y){
             puts("EXIT...");
             exit(0);
             break;
+        //control de lados
         case 51:
             lados = 3;
         break;
@@ -83,6 +81,19 @@ void keyboard(unsigned char key, int x, int y){
         break;
         case 57:
             lados = 9;
+        break;
+        //control de escalado
+        case 119: //letra w "minuscula"
+            if(sx >= 2.0)
+                break;
+            sx += 0.1;
+            sy += 0.1;
+        break;
+        case 115: //letra s "minuscula"
+            if(sx <= 0.1)
+                break;
+            sx -= 0.1;
+            sy -= 0.1;
         break;
     }//switch
     glutPostRedisplay(); //Mark the normal plane of current window as needing
@@ -110,7 +121,7 @@ int main(int argc, char **argv){
   //Buffer simple
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB ); //inicia el modo de visualizacion del programa
   glutInitWindowPosition(20,20); //en donde aparecera la esquina sup izquierda
-  glutInitWindowSize(500,500); //tamaño de la ventana en ancho y alto, pixeles
+  glutInitWindowSize(640, 640); //tamaño de la ventana en ancho y alto, pixeles
   glutCreateWindow("Poligono"); //lanza la ventana
   //Llamada a la función de dibujado
   glutDisplayFunc(display); //OpenGL se refresca solito
